120e5987d5bd7d34dae9f0b1f9e22c5de5ff4757
lcjingdi <lcjingdi@gmail.com>
2018-11-28 19:13:53 +0800

    图片列表

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -22,6 +22,22 @@
 </template>
 
 <script>
+export default {
+  data: function() {
+    return {
+      cates: [],
+      list: []
+    };
+  },
+  mounted: function() {
+    mui(".mui-scroll-wrapper").scroll({
+      deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+  },
+  created: function() {
+    this.getAllCategory();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -13,12 +13,15 @@ import "./lib/mui/css/mui.min.css";
 import "./lib/mui/css/icons-extra.css";
 
 
+// import 'mint-ui/lib/style.css'
+// import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
+// Vue.component(Header.name, Header);
+// Vue.component(Swipe.name, Swipe);
+// Vue.component(SwipeItem.name, SwipeItem);
+// Vue.component(Button.name, Button);
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
 import 'mint-ui/lib/style.css'
-import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
-Vue.component(Header.name, Header);
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
-Vue.component(Button.name, Button);
 
 import router from "./router.js";
 import app from "./App.vue";
